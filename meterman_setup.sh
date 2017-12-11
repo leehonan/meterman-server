d0056c26fb96fc8e0315c50d0a5332da879be192
Lee <lee@leehonan.com>
2017-12-12 00:14:44 +1100

    no message

--- meterman_setup.sh
+++ meterman_setup.sh
@@ -8,7 +8,16 @@ fi
 
 echo "Fetching prerequisites..."
 apt update
-apt install wget minicom sqlite3 gunzip avrdude libffi-dev libssl-dev zlib1g-dev build-essential checkinstall libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
+apt install wget screen minicom sqlite3 avrdude libffi-dev libssl-dev zlib1g-dev build-essential checkinstall libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
+
+echo "Done\n"
+echo "Setting up Python 3.6..."
+
+cd /home/pi/temp
+wget https://www.python.org/ftp/python/3.6.0/Python-3.6.0.tgz
+tar xzvf Python-3.6.0.tgz
+cd Python-3.6.0/
+./configure && sudo make -j4 && sudo make install
 
 echo "Done\n"
 echo "Setting up gateway..."
@@ -40,15 +49,6 @@ chmod 644 /lib/systemd/system/pishutdown.service
 systemctl daemon-reload
 systemctl enable pishutdown.service
 systemctl start pishutdown.service
-
-echo "Done\n"
-echo "Setting up Python 3.6..."
-
-cd /home/pi/temp
-wget https://www.python.org/ftp/python/3.6.0/Python-3.6.0.tgz
-tar xzvf Python-3.6.0.tgz
-cd Python-3.6.0/
-./configure && sudo make -j4 && sudo make install
 
 echo "Done\n"
 echo "Setting up meterman..."
